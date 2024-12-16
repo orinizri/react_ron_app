8e7e509e976c8f2fee74aacb81f21e1b74ef9bca
Ori Kanner <orikanner@Mac.lan>
2024-12-16 14:03:28 +0200

    upload files - collect errors, filter valid files by type and size

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const FILE_SIZE_LIMIT = 10_000;
+export const UPLOAD_FILES_VALID_TYPES = [
+    "application/pdf",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+]
--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -3,29 +3,51 @@ import Input from "../../components/input/input";
 import Form from "../../components/form/form";
 import Button from "../../components/button/button";
 import "./home.css";
+import { FILE_SIZE_LIMIT, UPLOAD_FILES_VALID_TYPES } from "../../utils/constants";
 
 function Home() {
   const { pending } = useFormStatus();
 
   function handleForm(e) {
-    e.preventDefault();
-    const filesList = e.target[0].files;
-    console.log(filesList);
-    // TODO: Limit files size (discuss about absolute boundary with Ron)
-    // TODO: Check base64 file types are congruent
-    // TODO: Handle exceptions
+    try {
+      e.preventDefault();
+      const filesList = Array.from(e.target[0].files);
+      console.log(filesList);
+      const errors = [];
+      if (!filesList.length) return;
+      // Filter only relevant files
+      const validFiles = filesList.filter((file) => {
+        // Filter files above 10mb
+        if (file.size > FILE_SIZE_LIMIT) {
+          errors.push({
+            fileName: file.name,
+            error: `File is above 10MB (${file.size} bytes)`,
+          });
+          return false; // Exclude this file
+        }
+        // Filter files not word/excel/pdf -
+        if (UPLOAD_FILES_VALID_TYPES.includes(file.type)) {
+          errors.push({
+            fileName: file.name,
+            error: `File is above 10MB (${file.size} bytes)`,
+          });
+          return false;
+        }
+        return true; // Include valid files
+      });
 
-    // It seems that I'm getting all the files without nested lists, I will need to differentiate between folders
-    // by file.webkitRelativePath which holds a relative path of the selected folder (the root)
+      if (errors.length) {
+        // Show errors (floating)
+        console.log("errors:", errors)
+      }
+      // TODO: Send validFiles to backend for analysis and aggregation
+      // (summing the content of the folder and the progress for each customer)
 
-    // Filter only relevant files
-    for (let file of filesList) {
-      // const fileName = file.name;
-      // const fileType = file.type;
+      // It seems that I'm getting all the files without nested lists, I will need to differentiate between folders
+      // by file.webkitRelativePath which holds a relative path of the selected folder (the root)
+    } catch (error) {
+      console.error(error);
     }
-
-    // FUTURE: Backend analysis of the files and return of an excel file
-    // summing the content of the folder and the progress with each customer
   }
 
   return (
